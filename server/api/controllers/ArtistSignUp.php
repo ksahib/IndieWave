812bbfa61e417f72c1d32a2862d134d7821db5e0
Kazi Sahib <161735924+ksahib@users.noreply.github.com>
2024-11-13 08:29:37 +0600

    Handled image upload

--- server/api/controllers/ArtistSignUp.php
+++ server/api/controllers/ArtistSignUp.php
@@ -4,14 +4,17 @@ header("Access-Control-Allow-Methods: POST, GET, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type, Authorization");
 include_once $_SERVER['DOCUMENT_ROOT'] . '/indiewave/api/config/database.php';
 include_once $_SERVER['DOCUMENT_ROOT'] . '/indiewave/api/models/artistmodel.php';
+include_once $_SERVER['DOCUMENT_ROOT'] . '/indiewave/api/models/imagemodel.php';
 include_once 'BaseController.php';
 
 class ArtistSignUp extends BaseController {
     private $artistModel;
+    private $imagemodel;
 
     public function __construct($db) {
         parent::__construct($db);
         $this->artistModel = new artistmodel($this->db);
+        $this->imagemodel = new imagemodel($this->db);
     }
 
     public function create() {
@@ -23,10 +26,25 @@ class ArtistSignUp extends BaseController {
             $this->validateRequiredFields($data, ['email', 'artist_name', 'about']);
             
             // Set default values for fields that are not provided
-            $data['profile_pic'] = 'default';
+            if($data['profile_pic'] === '') {
+                $data['profile_pic'] = 'default';
+            }
+            $imageid = uniqid();
+            $info = [
+                'image_id' => $imageid,
+                'image_url' => $data['profile_pic'],
+                'image_type' => 'profile_pic',
+            ];
+            if($this->imagemodel->create($info))
+            {
+                $data['profile_pic'] = $imageid;
+            }
+            else
+            {
+                $this->sendResponse(500, "Failed to upload image");
+                echo json_encode(['images'=>$info]);
+            }
             $data['follower_count'] = 0;
-            //$data['password'] = password_hash($data['password'], PASSWORD_BCRYPT);
-            //$this->sendResponse(201, 'Artist profile creating');
 
             // Call the ArtistModel's create method to insert into the database
             if ($this->artistModel->create($data)) {
