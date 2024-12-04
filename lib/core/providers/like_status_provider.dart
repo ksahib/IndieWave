b04e8486b5885f6accf9758117ce51a27a046667
Kazi Sahib <161735924+ksahib@users.noreply.github.com>
2024-12-04 19:53:53 +0600

    Delete lib directory

--- lib/core/providers/like_status_provider.dart
+++
@@ -1,4 +0,0 @@
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-
-// Define the likeStatusProvider
-final likeStatusProvider = StateProvider<bool>((ref) => false); // Initial value is false (not liked)
